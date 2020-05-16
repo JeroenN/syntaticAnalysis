09b63a114fb1efb52c7d2a6c742ba482bc5e537c
mathijs niemendal <jcaniemendal@gmail.com>
2020-05-16 22:52:57 +0200

    added syntatic analysis of four differnt types of questions

--- s3799042.py
+++ s3799042.py
@@ -3,12 +3,27 @@ from __future__ import unicode_literals, print_function
 import plac
 import spacy
 import requests
-from nltk import Tree
 
 def main():
+    nlp = spacy.load("en_core_web_sm")
     questions = create_questions()
-    run_questions(questions)
+    for question in questions:
+        print(question)
+        question = nlp(question)
+        #print_dep(question)
+        run_different_formulations(question)
 
+def print_dep(question):
+    for word in question:
+        print(word.text + ": " + word.dep_)
+
+
+def lemmafy(question):
+    newQuestion = ""
+    for word in question:
+        newQuestion += word.lemma_
+        newQuestion += " "
+    return newQuestion
 
 def debug(question):
     for word in question:
@@ -18,31 +33,76 @@ def debug(question):
             print("pobj:  " + word.text + "   entity:" + "".join(w.text_with_ws for w in word.subtree))
 
 
-def get_property_word(question):
-    property=""
-    #for i in range(len(question)):
+def get_property_word_nsubj(question):
+    property = ""
+    for word in question:
+        if word.dep_ in "nsubj":
+            for w in word.lefts:
+                if not w.dep_ in ("det", "pobj", "prep"):
+                    property += "".join(w.text_with_ws)
+            property+=word.text
+    return property
+
+def get_entity_word_pobj(question):
+    entity = ""
+    for word in question:
+        if word.dep_ in ("pobj"):
+            for w in word.subtree:
+                if not w.dep_ in ("det"):
+                    entity += "".join(w.text_with_ws)
+    return entity
+
+def get_entity_word_nsubj(question):
+    entity = ""
     for word in question:
         if word.dep_ in ("nsubj"):
-            property = word.text
-            print("property: " + property)
-            return property
-    if not property:
-        print("no property was found")
+            for w in word.subtree:
+                if not w.dep_ in ("det"):
+                    entity += "".join(w.text_with_ws)
+    return entity
 
-def get_entity_word(question):
-    entity=""
-    b=[]
+def get_entity_word_dobj(question):
+    entity = ""
     for word in question:
+        if word.dep_ in ("dobj"):
+            for w in word.subtree:
+                if not w.dep_ in ("det"):
+                    entity += "".join(w.text_with_ws)
+    return entity
 
-        if word.dep_ in ("pobj"):
-            for a in word.subtree:
-                if not a.dep_ in ("det"):
-                    b.append = a
-            entity = "".join(w.text_with_ws for w in b)
-            print("entity: " + entity)
-            return entity
-    if not entity:
-        print("no entity was found")
+def get_property_word_attr(question):
+    property = ""
+    for word in question:
+        if word.dep_ in ("attr") and not word.tag_ in "WP":
+            property+=word.text
+    return property
+
+def get_property_word_root(question):
+    property = ""
+    for word in question:
+        if word.dep_ in ("ROOT"):
+            property+=word.text
+    return property
+
+def get_entity_word_compound(question):
+    entity = ""
+    for word in question:
+        if word.dep_ in ("ROOT"):
+            for w in word.lefts:
+                for a in w.subtree:
+                    if not a.dep_ in ("det", "aux", "dobj"):
+                        entity += "".join(a.text_with_ws)
+    return entity
+
+def get_entity_word_poss(question):
+    entity = ""
+    for word in question:
+        if word.dep_ in ("poss"):
+            for w in word.lefts:
+                if not w.dep_ in ("det"):
+                    entity += "".join(w.text_with_ws)
+            entity += word.text
+    return entity
 
 def get_entity_codes(entity):
     url = 'https://www.wikidata.org/w/api.php'
@@ -54,9 +114,59 @@ def get_entity_codes(entity):
         arrEntityCodes.append(result['id'])
 
     if not arrEntityCodes:
-        print("Entity error: no '" + entity +"' found")
+        print("Entity error: no '" + entity + "' found")
         print("The program could not find an answer to this question. \n")
     return arrEntityCodes
+
+#What is the taxon name of a wolf
+def zero(question):
+    propertyWord = get_property_word_nsubj(question)
+    entityWord = get_entity_word_pobj(question)
+    if propertyWord and entityWord and propertyWord !="Who":
+        return run_question(propertyWord, entityWord)
+
+#Who are the childeren of Albert Einstein
+def one(question):
+    propertyWord = get_property_word_attr(question)
+    entityWord = get_entity_word_pobj(question)
+    if propertyWord and entityWord:
+        return run_question(propertyWord, entityWord)
+
+#Who has invented the world wide web
+def two(question):
+    propertyWord = get_property_word_root(question)
+    entityWord = get_entity_word_dobj(question)
+    if propertyWord and entityWord:
+        return run_question(propertyWord, entityWord)
+
+#Who did invent the world wide web
+def three(question):
+    propertyWord = get_property_word_root(question)
+    entityWord = get_entity_word_compound(question)
+    if propertyWord and entityWord:
+        return run_question(propertyWord, entityWord)
+
+#Who was nikola Tesla's employer
+def four(question):
+    propertyWord = get_property_word_attr(question)
+    entityWord = get_entity_word_poss(question)
+    if propertyWord and entityWord:
+        return run_question(propertyWord, entityWord)
+
+def run_different_formulations(question):
+    switcher = {
+        0: zero(question),
+        1: one(question),
+        2: two(question),
+        3: three(question),
+        4: four(question)
+    }
+    for i in range(len(switcher)):
+        answer = switcher.get(i, "not an formulation type")
+        if answer:
+            print(i)
+            print(answer)
+            #i = len(switcher)
 
 def get_property_codes(property):
     url = 'https://www.wikidata.org/w/api.php'
@@ -70,6 +180,7 @@ def get_property_codes(property):
 
     return arrPropertycodes
 
+
 def create_query(entity, property):
     query = '''
         SELECT ?answer ?answerLabel WHERE{
@@ -80,12 +191,13 @@ def create_query(entity, property):
     }'''
     return query
 
+
 def run_query(query):
     headers = {
         'User-Agent': 'Mozilla/5.0 (x11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/527.36'
     }
     url = 'https://query.wikidata.org/sparql'
-    data = requests.get(url, headers= headers, params={'query': query, 'format': 'json'}).json()
+    data = requests.get(url, headers=headers, params={'query': query, 'format': 'json'}).json()
     final_results = []
     for item in data['results']['bindings']:
         final_results.append(item['answerLabel']['value'])
@@ -94,55 +206,30 @@ def run_query(query):
 def print_answer(answer):
     print(answer)
 
+
 def create_questions():
     questions = []
-    questions.append("Who are the crew members of the Apollo 11?")
-    questions.append("Who is the inventor of the automobile?")
-    questions.append("What are the parts of the lungs?")
+    #questions.append("What did Alexander Fleming invent?")
+    questions.append("Who was Nikola Tesla's employer?")
+    questions.append("Who did invent the world wide web")
+    questions.append("Who has invented the world wide web")
+    questions.append("Who invented penicillin?")
+    questions.append("Who are the children of Albert Einstein?")
     questions.append("What are the symptoms of the common cold?")
     questions.append("What is the atomic number of silver?")
-    questions.append("What is the boiling point of alcohol?")
-    questions.append("What is the birth date of Maria Skłodowska?")
     questions.append("What is the anti particle of an electron?")
     questions.append("What is the taxon name of a wolf?")
-    questions.append("Who are the children of Albert Einstein?")
+    questions.append("What did Alexander Fleming invent?")
     return questions
 
-def tok_format(tok):
-    return "-".join([tok.orth_, tok.tag_, tok.dep_])
-
-
-def to_nltk_tree(node):
-    if node.n_lefts + node.n_rights > 0:
-        return Tree(tok_format(node), [to_nltk_tree(child) for child in node.children])
-    else:
-        return tok_format(node)
-
-def run_questions(questions):
-    nlp = spacy.load("en_core_web_sm")
-    for i in range(len(questions)):
-        line = questions[i]
-        print(line)
-        line = nlp(line)
-        #[to_nltk_tree(sent.root).pretty_print() for sent in line.sents]
-
-        propertyWord = get_property_word(line)
-        entityWord = get_entity_word(line)
-        if(entityWord == "the lungs"):
-            entityWord = "lungs"
-        if propertyWord and entityWord:
-            arrPropertycodes = get_property_codes(propertyWord)
-            arrEntitycodes = get_entity_codes(entityWord)
-            if arrEntitycodes and arrPropertycodes:
-                    entityCode =arrEntitycodes[0]
-                    propertyCode = arrPropertycodes[0]
-                    query = create_query(entityCode, propertyCode)
-                    answer = run_query(query)
-                    if answer:
-                        print_answer(answer)
-                    else:
-                        print("Could not find an answer \n")
-
+def run_question(propertyWord, entityWord):
+    arrPropertycodes = get_property_codes(propertyWord)
+    arrEntitycodes = get_entity_codes(entityWord)
+    if arrEntitycodes and arrPropertycodes:
+        entityCode = arrEntitycodes[0]
+        propertyCode = arrPropertycodes[0]
+        query = create_query(entityCode, propertyCode)
+        return run_query(query)
 
 if __name__ == "__main__":
     plac.call(main)
